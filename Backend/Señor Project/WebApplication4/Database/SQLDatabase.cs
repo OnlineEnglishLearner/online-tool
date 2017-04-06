4635aa4ff46ac850334db9e5958fc6bdbb1f31bf
Sahir Boghani <sahirboghani@gmail.com>
2017-04-06 17:51:20 -0400

    async db entry

--- Backend/Señor Project/WebApplication4/Database/SQLDatabase.cs
+++ Backend/Señor Project/WebApplication4/Database/SQLDatabase.cs
@@ -2,11 +2,7 @@
 using System;
 using System.Data;
 using System.Diagnostics;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using System.Collections.Generic;
 
 public class SQLDatabase
@@ -19,17 +15,37 @@ public class SQLDatabase
     static string[] returnString;
     static string contentString;
 
+    // cache latest 5 passages
     static Cache cache = new Cache();
-    
+
+    // server can hold list of all titles
+    static HashSet<string> titles = new HashSet<string>();
+
     public static bool addPassage(string title, string content)
     {
-        if (cache.ContainsKey(title))
+        if (titles.Contains(title))
             return false;
 
         cache.Add(title, content);
-
+        titles.Add(title);
+        
         return true;
-        /*
+    }
+
+    public static string getPassage(string title)
+    {
+        string res;
+
+        if (cache.ContainsKey(title))
+            res = cache.Get(title);
+        else
+            res = "Title does not exist";
+
+        return res;
+    }
+
+    public static bool titleExists(string title)
+    {
         try
         {
             if (connection.State == ConnectionState.Closed)
@@ -38,8 +54,47 @@ public class SQLDatabase
                 connection.Open();
 
                 title = title.Replace("'", "\\'");
+                
+                int passageCount = 0;
+                query = "SELECT COUNT(*) FROM Passages WHERE title = '" + title + "'";
+                cmd = new MySqlCommand(query, connection);
 
+                object result = cmd.ExecuteScalar();
 
+                if (result != null)
+                    passageCount = Convert.ToInt32(result);
+                else
+                    return false;
+
+                // title already taken
+                return passageCount == 0;
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Error: {0}", e.ToString());
+            returnString = null;
+        }
+        finally
+        {
+            if (connection != null)
+                connection.Close();
+        }
+
+        return false;
+    }
+
+    public static async Task<bool> addPassageAsync(string title, string content)
+    {
+        try
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+
+                connection.Open();
+
+                title = title.Replace("'", "\\'");
+                
                 int passageCount = 0;
                 query = "SELECT COUNT(*) FROM Passages WHERE title = '" + title + "'";
                 cmd = new MySqlCommand(query, connection);
@@ -75,61 +130,11 @@ public class SQLDatabase
                 connection.Close();
         }
 
-        return false;*/
-    }
-
-    public static bool titleExists(string title)
-    {
-        try
-        {
-            if (connection.State == ConnectionState.Closed)
-            {
-
-                connection.Open();
-
-                title = title.Replace("'", "\\'");
-
-
-                int passageCount = 0;
-                query = "SELECT COUNT(*) FROM Passages WHERE title = '" + title + "'";
-                cmd = new MySqlCommand(query, connection);
-
-                object result = cmd.ExecuteScalar();
-
-                if (result != null)
-                    passageCount = Convert.ToInt32(result);
-                else
-                    return false;
-
-                // title already taken
-                return passageCount == 0;
-            }
-        }
-        catch (Exception e)
-        {
-            Debug.WriteLine("Error: {0}", e.ToString());
-            returnString = null;
-        }
-        finally
-        {
-            if (connection != null)
-                connection.Close();
-        }
-
         return false;
     }
 
-    public static string getPassage(string title)
+    public static async Task<string> getPassageAsync(string title)
     {
-        string res;
-
-        if (cache.ContainsKey(title))
-            res = cache.Get(title);
-        else
-            res = "Title does not exist";
-
-        return res;
-        /*
         try
         {
             if (connection.State == ConnectionState.Closed)
@@ -176,6 +181,5 @@ public class SQLDatabase
         }
 
         return contentString;
-    */
     }
 }
